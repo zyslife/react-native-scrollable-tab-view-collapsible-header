1e751b829bde141217d4f15c4b979e5c162e7161
zou yu <zouyu@benewtech.cn>
2021-03-22 02:13:43 +0800

    1.0.0

--- SlideTabView.tsx
+++
@@ -1,3 +0,0 @@
-import { createHeaderTabsComponent } from './index'
-import ScrollableTabView from 'react-native-scrollable-tab-view'
-export default createHeaderTabsComponent(ScrollableTabView, { slideAnimated: true })
