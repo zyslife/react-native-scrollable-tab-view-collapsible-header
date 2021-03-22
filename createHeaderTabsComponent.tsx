b46e28dfee2c7380ddae3f5c7b6704086b5f0d59
zou yu <zouyu@benewtech.cn>
2021-03-22 10:28:59 +0800

    Resolve the conflict

--- createHeaderTabsComponent.tsx
+++ createHeaderTabsComponent.tsx
@@ -17,42 +17,8 @@ export default function createHeaderTabsComponent(Component: typeof ScrollableTa
 
 }
 
-<<<<<<< HEAD
 const CollapsibleHeaderTabView: React.FC<ForwardTabViewProps> = (props: ForwardTabViewProps) => {
     const [currentIndex, setCurrentIndex] = useState(props.initialPage || 0)
-=======
-class CollapsibleHeaderTabView extends React.Component<ForwardTabViewProps, { currentIndex: number }>{
-
-    constructor(props: ForwardTabViewProps) {
-        super(props)
-        this.state = {
-            currentIndex: props.initialPage || 0
-        }
-    }
-
-    renderTabViewContainer = (props: TabViewContainerBaseProps) => {
-        const collapsibleParams: CollapsibleHeaderProps = {
-            frozeTop: this.props.frozeTop,
-            tabbarHeight: this.props.tabbarHeight,
-            overflowHeight: this.props.overflowHeight,
-            scrollEnabled: this.props.scrollEnabled,
-            isRefreshing: this.props.isRefreshing,
-            refreshHeight: this.props.refreshHeight,
-            makeRoomInRefreshing: this.props.makeRoomInRefreshing,
-            onStartRefresh: this.props.onStartRefresh,
-            makeScrollTrans: this.props.makeScrollTrans,
-            makeHeaderHeight: this.props.makeHeaderHeight,
-            renderScrollHeader: this.props.renderScrollHeader,
-            renderRefreshControl: this.props.renderRefreshControl
-        }
-
-        return <FitTabView
-            {...collapsibleParams}
-            {...props}
-            currentIndex={this.state.currentIndex}
-            renderTabView={this.renderTabView} />
-    }
->>>>>>> fd9e73fdbb5a7c9ea6d8706c76dc2a95fa4eaa94
 
     const _onChangeTab = (e: any) => {
         props.onChangeTab && props.onChangeTab(e)
